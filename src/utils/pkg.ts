58b021398f3526e4dad95f4e9e1695d71ec5ddca
sina-byn <sina.sina.bayan@protonmail.com>
2025-02-16 21:48:21 +0330

    refactor: package.json related utils logic and funcs refactored

--- src/utils/pkg.ts
+++ src/utils/pkg.ts
@@ -6,12 +6,11 @@ const PACKAGE_PATH = 'package.json';
 // * types
 type PackageJson = { [k: string]: unknown };
 
-export const loadPkgJson = () => {
+export const loadPkgJson = (): PackageJson => {
   if (!fs.existsSync(PACKAGE_PATH)) throw new Error(`Could not find '${PACKAGE_PATH}'`);
-  const pkg: PackageJson = JSON.parse(fs.readFileSync(PACKAGE_PATH, 'utf-8'));
+  return JSON.parse(fs.readFileSync(PACKAGE_PATH, 'utf-8'));
+};
 
-  return {
-    set: (key: keyof PackageJson, value: unknown) => (pkg[key] = value),
-    save: () => fs.writeFileSync(PACKAGE_PATH, JSON.stringify(pkg, null, 2), 'utf-8'),
-  };
+export const savePkgJson = (pkg: PackageJson) => {
+  fs.writeFileSync(PACKAGE_PATH, JSON.stringify(pkg, null, 2), 'utf-8');
 };
