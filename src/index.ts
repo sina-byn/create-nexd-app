3f0dc37018b2a942c0c513cfba6e76f96cb3f86e
sina-byn <sina.sina.bayan@protonmail.com>
2025-02-16 12:00:33 +0330

    refactor: logging and error handling refactored

--- src/index.ts
+++ src/index.ts
@@ -13,15 +13,13 @@ import { program } from 'commander';
 process.on('uncaughtException', (err: Error) => {
   const errMessage = err.stack ?? `${err.name}: ${err.message}`;
   console.error(pc.redBright('\n' + errMessage));
+  console.error(pc.redBright('❌ Failed initializing Nexd app'));
 
   process.exit(1);
 });
 
 const createApp = async () => {
-  // * ora
   const ora = (await import('ora')).default;
-
-  // * execa
   const { $ } = await import('execa');
 
   const { appName } = await prompts({
@@ -33,11 +31,11 @@ const createApp = async () => {
 
   const appDir = appName === '.' ? process.cwd() : path.join(process.cwd(), appName);
 
-  const exists = fs.existsSync(appDir);
-  if (!exists) fs.mkdirSync(appDir, { recursive: true });
+  if (fs.existsSync(appDir) && fs.readdirSync(appDir).length > 0) {
+    throw new Error(`'${appDir}' is not an empty directory`);
+  }
 
-  const isEmpty = exists ? fs.readdirSync(appDir).length === 0 : true;
-  if (!isEmpty) throw new Error(`'${appDir}' is not an empty directory`);
+  fs.mkdirSync(appDir, { recursive: true });
 
   const { gitInit, npmInstall } = await prompts([
     {
@@ -56,24 +54,24 @@ const createApp = async () => {
 
   const emitter = degit('sina-byn/node-color-picker');
 
-  emitter.on('info', info => {
-    const message = info.message[0].toUpperCase() + info.message.slice(1);
-    spinner.succeed(pc.greenBright(message));
-  });
-
   await emitter.clone(appDir);
+  spinner.succeed(pc.greenBright('Template cloned successfully'));
 
   process.chdir(appDir);
 
-  if (gitInit) await $({ stdio: 'inherit' })`git init`;
+  if (gitInit) {
+    spinner.start(pc.blueBright('Initializing Git repository...\n'));
+    await $({ stdio: 'inherit' })`git init`;
+    spinner.succeed(pc.greenBright('Git repository initialized'));
+  }
 
   if (npmInstall) {
     spinner.start(pc.blueBright('Installing dependencies...'));
-
     await $`npm i`;
-
     spinner.succeed(pc.greenBright('Dependencies installed successfully'));
   }
+
+  console.log(pc.greenBright('✅ Nexd app initialized successfully'));
 };
 
 program.action(async () => await createApp());
